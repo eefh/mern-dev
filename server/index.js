23c8cc732c753b07ecb7458d1c5ccfe3d09620a5
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-09-10 20:16:21 -0400

    Setup express and mongodb

--- server/index.js
+++ server/index.js
@@ -2,4 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://eefjh:data123@cluster0.pb937wg.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+        .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}!`)))
+        .catch((error) => console.log(error));
